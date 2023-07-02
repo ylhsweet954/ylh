1bd4ab32cde843ba7ca58ac934371797410cc81e
ylh <ylhsweet954@gmail.com>
2023-07-02 23:50:23 +0800

    修改图标和标题，删除无用代码

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,27 +1,12 @@
 <template>
   <div class="container">
     <Header></Header>
-    <!-- <h1>welcome to my project!</h1> -->
-    <el-dropdown>
-      <el-button type="primary">
-        Dropdown List
-      </el-button>
-      <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item>Action 1</el-dropdown-item>
-          <el-dropdown-item>Action 2</el-dropdown-item>
-          <el-dropdown-item>Action 3</el-dropdown-item>
-          <el-dropdown-item>Action 4</el-dropdown-item>
-          <el-dropdown-item>Action 5</el-dropdown-item>
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
+    <h1>welcome to my project!</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
-
 
 </script>
 
