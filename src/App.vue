104183f68fb8814e9315974de48688e28174f217
ylh <ylhsweet954@gmail.com>
2023-09-10 22:34:24 +0800

    fix: 删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,6 @@
 </template>
 
 <script setup lang="ts">
-// import HelloWorld from "./components/HelloWorld.vue";
-// import { useStore } from 'vuex'
-// import { ref } from 'vue'
-// const store = useStore()
-// console.log(store.state)
-// const msg = ref('海映红是笨蛋')
-// const change = () => {
-//   msg.value = '海映红是猪'
-// }
-
 </script>
 
 
