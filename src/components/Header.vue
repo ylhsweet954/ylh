b02f6ddd31760f1900a0b6ba956f74b9bec1c20b
ylh <ylhsweet954@gmail.com>
2023-09-10 23:04:47 +0800

    feat: 无

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
       <el-menu-item index="1">开始</el-menu-item>
       <el-sub-menu index="2">
-        <template #title>测试</template>
+        <template #title>组件使用</template>
         <el-menu-item index="2-1">拖拽</el-menu-item>
         <el-menu-item index="2-2">item two</el-menu-item>
         <el-menu-item index="2-3">item three</el-menu-item>
