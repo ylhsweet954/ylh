b4f91d348099ed377998a66c177e71d882ec6fcc
ylh <ylhsweet954@gmail.com>
2023-09-10 22:20:52 +0800

    feat: 调整依赖
    20230910

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
 import { resolve } from "path";
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"), // 设置 `@` 指向 `src` 目录
